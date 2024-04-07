be707ccd6d56eb136c0160eb302531a6d2ffacbb
SeLvaChandirasekr <154896256+SeLvaChandirasekr@users.noreply.github.com>
2024-04-07 09:10:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ http_server.get("/hall-booking",(req,res,next)=> {
 
 
 // starts a simple http server locally on port 3000
-http_server.listen(3000, '127.0.0.1', () => {
+http_server.listen('0.0.0.0:3000', '127.0.0.1', () => {
   console.log('Listening on 127.0.0.1:3000');
 });
 
